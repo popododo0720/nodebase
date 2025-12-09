7fcecd3642b17b56d27a2ae09ecee19f4bedb905
kim soo <skim@coremaxtech.com>
2025-12-09 16:33:51 +0900

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+"use client";
+
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from 'sonner';
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());  
 
-  const data = await caller.getUsers();
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued");
+    }
+  }));
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
@@ -13,6 +24,9 @@ const Page = async () => {
       <div>
         {JSON.stringify(data, null, 2)}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,20 @@
+import { inngest } from '@/inngest/client';
 import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 
 export const appRouter = createTRPCRouter({
-    getUsers: protectedProcedure.query(({ ctx }) => {
-        return prisma.user.findMany();
+    getWorkflows: protectedProcedure.query(({ ctx }) => {
+        return prisma.workflow.findMany();
+    }),
+    createWorkflow: protectedProcedure.mutation(async () => {
+        await inngest.send({
+            name: "test/hello.world",
+            data: {
+                email: "test@test.com",
+            },
+        });
+
+        return { success: true, message: "Job queued" }
     }),
 });
 
